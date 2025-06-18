50108ff706669a1b52e31aed65aae74e5ebbfb31
aksayskaya2804 <aksayskaya2804@gmail.com>
2025-06-18 13:27:58 +0300

    Create controllers, requests and route

---
+++ app/Http/Controllers/OrderController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreOrderRequest;
+use App\Models\Order;
+use App\Models\Product;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+
+class OrderController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
+     */
+    public function index()
+    {
+        $orders = Order::with('products')->get();
+        return view('orders.index', compact('orders'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
+     */
+    public function create()
+    {
+        $products = Product::all();
+        return view('orders.create', compact('products'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param StoreOrderRequest $request
+     * @return RedirectResponse
+     */
+    public function store(StoreOrderRequest $request)
+    {
+        Order::create($request->validated());
+        return redirect()->route('orders.index')->with('success', 'Заказ создан');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param Order $order
+     * @return Application|Factory|View
+     */
+    public function show(Order $order)
+    {
+        return view('orders.show', compact('order'));
+    }
+
+    //TODO написать метод для смены статуса
+}
